40c40bd30634f2103965879985bfd5cc0cf74342
torgal <torgal@7a2de714-0123-0410-93ad-e5d03f72b922>
2007-09-01 14:32:50 +0000

    improved mainboot script

--- mainboot.sh
+++ mainboot.sh
@@ -1,51 +1,53 @@
 #!/bin/sh
 
 RESULT=53
+STOP_REASON="initial bootup"
 
 ulimit -c unlimited
 
 while [ $RESULT != 0 ]; do
+	DATESTR=`date +%C%y.%m.%d-%H.%M.%S`
+
   if [ $RESULT != 52 ]; then
     if [ -f src/dms_new ]; then
-			DATESTR=`date +%C%y-%m-%d_%H.%M.%S`
-		  mv dms dms.$DATESTR
+			if [ -f dms ]; then
+		  	mv dms dms.$DATESTR
+			fi
       mv src/dms_new dms
-    fi
-    if [ -f areas/tworld.wld ]; then
-      mv areas/tworld.wld areas/world.wld
-    fi
-    if [ -f areas/tworld.obj ]; then
-      mv areas/tworld.obj areas/world.obj
-    fi
-    if [ -f areas/tworld.zon ]; then
-      mv areas/tworld.zon areas/world.zon
-    fi
-    if [ -f areas/tworld.shp ]; then
-      mv areas/tworld.shp areas/world.shp
-    fi
-    if [ -f areas/tworld.qst ]; then
-      mv areas/tworld.qst areas/world.qst
     fi
   fi
 
   if [ -d logs/log ]; then
-    LOGNAME=`date +%b%d-%H%M`
-    mkdir logs/old-logs/$LOGNAME
-    mv logs/log/* logs/old-logs/$LOGNAME
+    #LOGNAME=`date +%b%d-%H%M`
+    mkdir logs/old-logs/$DATESTR
+    mv logs/log/* logs/old-logs/$DATESTR
   fi
-
-  #echo "Copying news files.."
-  #rm -rf /var/www/html/duris_files/*
-  #cp /duris/mud/lib/information/news /var/www/html/duris_files/
 
   echo "Creating svn log file.."
   /usr/bin/svn2cl -f lib/information/changelog.src src
 
-  echo "Generating list of functions.."
+  echo "Generating list of function names.."
   nm --demangle dms | grep " T " | sed -e 's/[(].*[)]//g' > lib/event_names
 
-  echo "Starting duris server.."
+	if [ -f /usr/local/bin/sendEmail ]; then
+		/usr/local/bin/sendEmail -t postmaster@angryturnip.com \
+			-f mud@durismud.com -u "Duris Booting..." \
+			-m "Mud booting at ${DATESTR}, previous shutdown reason: ${REASON}."
+	fi
+
+  echo "Starting duris..."
   ./dms 7777 > dms.out
 
+	# capture the exit code
   RESULT=${PIPESTATUS[0]}
+
+	# determine the reason for shutting down
+	case $RESULT in
+		0) STOP_REASON="shutdown";;
+		139) STOP_REASON="crash";;
+		52) STOP_REASON="copyover reboot";;
+		*) STOP_REASON="unknown";;
+	esac	
+
+	echo "Mud stopped, reason: ${STOP_REASON}"
 done
