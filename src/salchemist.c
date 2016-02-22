4d1bcfa8a8853fd7c88a3adbd557c53968f86de4
Lohrr <lohrr@durismud.com>
2016-02-22 18:18:56 +0000

    Fixed old bad debugging messages for encrust.

--- src/salchemist.c
+++ src/salchemist.c
@@ -846,6 +846,7 @@ void do_encrust(P_char ch, char *argument, int cmd)
   sprintf(buf2, "%s attempts to encrust %s with %s...", GET_NAME(ch),
           item->short_description, jewel->short_description);
   act(buf2, TRUE, ch, 0, 0, TO_ROOM);
+  wizlog(56, buf2 );
 
   craftsmanship = item->craftsmanship;
 //  if(!item->value[5] || !item->value[6] || !item->value[7])
@@ -869,7 +870,6 @@ void do_encrust(P_char ch, char *argument, int cmd)
   else
   {
     //notch_skill(ch, SKILL_ENCRUST, 7.7);
-    wizlog(56, "and created %s", item->short_description);
     act("...creating a real masterpiece!", TRUE, ch, 0, 0, TO_ROOM);
     act("Hurrah! Hurrah!", FALSE, ch, 0, 0, TO_CHAR);
   }
@@ -923,6 +923,8 @@ void do_encrust(P_char ch, char *argument, int cmd)
   extract_obj(item);
   extract_obj(jewel);
   obj_to_char(new_item, ch);
+
+  wizlog(56, "and created %s", new_item->short_description);
 
   return;
 }
