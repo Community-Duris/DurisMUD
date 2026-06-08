a70534e42a70c7fe9358088a203777a07b2f2d70
Adam Borowski <kilobyte@angband.pl>
2026-06-08 10:24:41 +0200

    Fix truncation of revdns names.

--- src/actinf.c
+++ src/actinf.c
@@ -6561,13 +6561,9 @@ void do_users(P_char ch, char *argument, int cmd)
 				if (f != NULL)
 				{
 					if (fscanf(f, "%s\n", hostbuf) == 1)
-					{
-						strncpy(d->host2, hostbuf, 128);
-					}
+						strlcpy(d->host2, hostbuf, sizeof d->host2);
 					else
-					{
-						strncpy(d->host2, d->host, 128);
-					}
+						strlcpy(d->host2, d->host, sizeof d->host2);
 
 					fclose(f);
 				}
@@ -6716,11 +6712,9 @@ void do_users_DEPRECATED(P_char ch, char *argument, int cmd)
 				if (f != NULL)
 				{
 					if (fscanf(f, "%s\n", buf) == 1)
-					{
-						strncpy(d->host2, buf, 128);
-					}
+						strlcpy(d->host2, buf, sizeof d->host2);
 					else
-						strncpy(d->host2, d->host, 128);
+						strncpy(d->host2, d->host, sizeof d->host2);
 					fclose(f);
 				}
 			}
@@ -6834,11 +6828,9 @@ void do_users_DEPRECATED(P_char ch, char *argument, int cmd)
 					if (f != NULL)
 					{
 						if (fscanf(f, "%s\n", buf) == 1)
-						{
-							strncpy(d->host2, buf, 128);
-						}
+							strlcpy(d->host2, buf, sizeof d->host2);
 						else
-							strncpy(d->host2, d->host, 128);
+							strncpy(d->host2, d->host, sizeof d->host2);
 						fclose(f);
 					}
 				}
