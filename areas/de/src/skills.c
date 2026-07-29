Community-Duris/Duris

commit 0c1c756ff73add9ffcb5dd83fba0889e18782ce9

Revert "- fixing bug with load_one_mine not extracting the mines it creates if it can't find a location for them"
This reverts commit d3ff8b5d49ce542920bf24188eba89ffa2616c46.

areas/de/src/common.c
@@ -1,1 +1,1 @@
-../../../src/common.c
+#include "../../../src/common.c"

areas/de/src/defines.h
@@ -1,1 +1,1 @@
-../../../src/defines.h
+#include "../../../src/defines.h"

areas/de/src/objmisc.h
@@ -1,1 +1,1 @@
-../../../src/objmisc.h
+#include "../../../src/objmisc.h"

areas/de/src/skills.c
@@ -1,1 +1,0 @@
-../../../src/skills.c

areas/de/src/skills.cpp
@@ -1,0 +1,1 @@
+#include "../../../src/skills.c"

areas/de/src/spells.h
@@ -1,1 +1,1 @@
-../../../src/spells.h
+#include "../../../src/spells.h"

Community-Duris/DurisMUD

commit 183cb8ecc1261520fa957cd25a4b0816174ff956

Unbreak DE; typechange symlinks vs files
Broken in 0c1c756ff73add9ffcb5dd83fba0889e18782ce9.

areas/de/src/skills.c
@@ -1,0 +1,1 @@
+#include "../../../src/skills.c"

areas/de/src/skills.cpp
@@ -1,1 +1,0 @@
-#include "../../../src/skills.c"